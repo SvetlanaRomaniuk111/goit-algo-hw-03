89eea126f5384a9c5763f680098571b8378fdea8
SvetlanaRomaniuk111 <gatskosv@gmail.com>
2024-09-23 12:57:28 +0300

    improved code without else

--- hw_03_task_1.py
+++ hw_03_task_1.py
@@ -4,11 +4,10 @@ def get_days_from_today(date):
     today = datetime.today()
     try:
         given_date = datetime.strptime(date, "%Y-%m-%d")
-        days_difference = (given_date - today).days           
+        days_difference = (given_date - today).days   
+        print(days_difference)        
     except ValueError as e:
-        print('Error: The date format must be: "YYYY-MM-DD"')
-    else:
-        print(days_difference)
+        print('Error: The date format must be: "YYYY-MM-DD"')        
             
 get_days_from_today("2024-02-05")
 get_days_from_today("2025-02-05")
