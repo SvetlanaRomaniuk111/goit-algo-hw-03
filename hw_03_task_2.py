d3d26784c4a196bc2051a5e05ac2b7f5abe2c73d
SvetlanaRomaniuk111 <gatskosv@gmail.com>
2024-09-23 12:40:33 +0300

    add comment to print

--- hw_03_task_2.py
+++ hw_03_task_2.py
@@ -5,5 +5,5 @@ def get_numbers_ticket(min, max, quantity):
 
 numbers_1 = get_numbers_ticket(1, 49, 6)
 numbers_2 = get_numbers_ticket(1, 36, 5)
-print(numbers_1)
-print(numbers_2)
+print(f'Six random numbers: {numbers_1}')
+print(f'Five random numbers: {numbers_2}')
