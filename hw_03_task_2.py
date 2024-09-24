a545ab963e7d7631087efa06839e74d9be9e33ba
SvetlanaRomaniuk111 <gatskosv@gmail.com>
2024-09-24 15:06:21 +0300

    second option

--- hw_03_task_2.py
+++ hw_03_task_2.py
@@ -13,3 +13,22 @@ print(get_numbers_ticket(1, 36, 5))
 print(get_numbers_ticket(10, 4, 2))
 print(get_numbers_ticket(10, 15, 9))
 print(get_numbers_ticket(-10, 5, 20))
+
+#-- Не знаю, як правильно, це другий варіант --
+
+import random
+
+def get_numbers_ticket(min:int, max:int, quantity:int) -> int:
+    list_of_numbers = []
+    if not max - min < quantity:    
+        if max - min > quantity:         
+            list_of_numbers = random.sample(range(min, max), quantity)
+            return sorted(list_of_numbers)
+    else:        
+        return list_of_numbers 
+    
+print(get_numbers_ticket(1, 49, 6))
+print(get_numbers_ticket(1, 36, 5))
+print(get_numbers_ticket(10, 4, 2))
+print(get_numbers_ticket(10, 15, 9))
+print(get_numbers_ticket(-10, 5, 20))
