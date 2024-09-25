07a047f52dd5bf6cf1856cfec218f11c4d0381fd
SvetlanaRomaniuk111 <gatskosv@gmail.com>
2024-09-25 19:44:48 +0300

    correct task 2

--- hw_03_task_2.py
+++ hw_03_task_2.py
@@ -2,10 +2,10 @@ import random
 
 def get_numbers_ticket(min:int, max:int, quantity:int) -> int:
     list_of_numbers = []
-    if max - min > quantity:         
+    if max - min + 1 > quantity:         
         list_of_numbers = random.sample(range(min, max), quantity)
         return sorted(list_of_numbers)
-    if max - min < quantity or max < min:         
+    if max - min < quantity:         
         return list_of_numbers 
     
 print(get_numbers_ticket(1, 49, 6))
@@ -13,6 +13,7 @@ print(get_numbers_ticket(1, 36, 5))
 print(get_numbers_ticket(10, 4, 2))
 print(get_numbers_ticket(10, 15, 9))
 print(get_numbers_ticket(-10, 5, 20))
+print(get_numbers_ticket(1, 5, 5))
 
 #-- Не знаю, як правильно, це другий варіант --
 
@@ -21,7 +22,7 @@ import random
 def get_numbers_ticket(min:int, max:int, quantity:int) -> int:
     list_of_numbers = []
     if not max - min < quantity:    
-        if max - min > quantity:         
+        if max - min + 1 > quantity:         
             list_of_numbers = random.sample(range(min, max), quantity)
             return sorted(list_of_numbers)
     else:        
@@ -32,3 +33,4 @@ print(get_numbers_ticket(1, 36, 5))
 print(get_numbers_ticket(10, 4, 2))
 print(get_numbers_ticket(10, 15, 9))
 print(get_numbers_ticket(-10, 5, 20))
+print(get_numbers_ticket(10, 15, 5))
